e444c7b44a80342e119962367ea9ecddd5125133
Ben Felder <172955+kajk@users.noreply.github.com>
2021-12-03 10:45:28 +0100

    03: Add part2 in python3

--- 03/diagnostics.py
+++ 03/diagnostics.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Iterable
+from typing import Iterable, Callable
 
 
 def yield_file(filename: str) -> Iterable[str]:
@@ -11,60 +11,72 @@ def yield_file(filename: str) -> Iterable[str]:
             yield line.strip()
 
 
-class OccuranceCounter:
-    occurances: dict[str, int]
+class OccurrencesCounter:
+    occurrences: dict[str, int]
 
     def __init__(self):
-        self.occurances = {}
+        self.occurrences = {}
 
     def __repr__(self):
-        return f'{self.occurances=}\n'
+        return f'{self.occurrences}'
 
     def increment(self, value):
-        self.occurances[value] = self.occurances.get(value, 0) + 1
+        self.occurrences[value] = self.occurrences.get(value, 0) + 1
 
     @property
     def most_common(self) -> str | None:
         result = None
         occ = None
-        for value, count in self.occurances.items():
+        for value, count in self.occurrences.items():
             if occ is None or count > occ:
                 occ = count
                 result = value
+            elif count == occ:
+                result = '1'
         return result
 
     @property
     def least_common(self) -> str | None:
         result = None
         occ = None
-        for value, count in self.occurances.items():
+        for value, count in self.occurrences.items():
             if occ is None or count < occ:
                 occ = count
                 result = value
+            elif count == occ:
+                result = '0'
         return result
 
 
-class SubDiagnostics:
-    lines_consumed: int = 0
-    rate: dict[int, OccuranceCounter] = {}
-
-    def consume_line(self, line: str):
+def calculate_occurrences(lines: list[str]) -> dict[int, OccurrencesCounter]:
+    rate: dict[int, OccurrencesCounter] = {}
+    for line in lines:
         for idx, value in enumerate(line):
             try:
-                counter = self.rate[idx]
+                counter = rate[idx]
             except KeyError:
-                counter = OccuranceCounter()
-                self.rate[idx] = counter
+                counter = OccurrencesCounter()
+                rate[idx] = counter
             counter.increment(value)
-            print(f'{counter.occurances=}')
-        print(f'{line=} {self.rate=}')
+        print(f'{line=} {rate=}')
+    return rate
+
+
+class SubDiagnostics:
+    lines: list[str] = []
+
+    def consume_line(self, line: str):
+        self.lines.append(line)
+
+    # part 1
 
     @property
     def gamma_rate_str(self) -> str:
-        rate = ''
-        for count in self.rate.values():
-            rate = rate + count.most_common
-        return rate
+        rate_value = ''
+        rate = calculate_occurrences(self.lines)
+        for count in rate.values():
+            rate_value = rate_value + count.most_common
+        return rate_value
 
     @property
     def gamma_rate(self):
@@ -72,31 +84,73 @@ class SubDiagnostics:
 
     @property
     def epsilon_rate_str(self):
-        rate = ''
-        for count in self.rate.values():
-            rate = rate + count.least_common
-        return rate
+        rate_value = ''
+        rate = calculate_occurrences(self.lines)
+        for count in rate.values():
+            rate_value = rate_value + count.least_common
+        return rate_value
 
     @property
     def epsilon_rate(self):
         return int(self.epsilon_rate_str, 2)
 
     @property
-    def diagnostics(self):
+    def part1_diagnostics(self):
         return f'gamma_rate={self.gamma_rate_str}/{self.gamma_rate} ' \
                f'epsilon_rate={self.epsilon_rate_str}/{self.epsilon_rate}'
 
     @property
-    def result(self):
+    def part1(self):
         return self.epsilon_rate * self.gamma_rate
+
+    # part 2
+
+    def _filter(self, condition: Callable[[OccurrencesCounter], str]):
+        filtered = self.lines
+        for idx in range(0, len(self.lines[0])):
+            value = calculate_occurrences(filtered)
+            most_common = condition(value[idx])
+            filtered = [line for line in filtered if line[idx] == most_common]
+            print(f'{idx=} {most_common=} lines={filtered}')
+            if len(filtered) == 1:
+                return filtered[0]
+
+    @property
+    def oxygen_generator_rating_str(self):
+        return self._filter(lambda c: c.most_common)
+
+    @property
+    def oxygen_generator_rating(self):
+        return int(self.oxygen_generator_rating_str, 2)
+
+    @property
+    def c02_scrubber_rating_str(self):
+        return self._filter(lambda c: c.least_common)
+
+    @property
+    def c02_scrubber_rating(self):
+        return int(self.c02_scrubber_rating_str, 2)
+
+    @property
+    def part2_diagnostics(self):
+        return f'oxygen_generator_rating={self.oxygen_generator_rating_str}/{self.oxygen_generator_rating} ' \
+               f'co2_scrubber_rating={self.c02_scrubber_rating_str}/{self.c02_scrubber_rating}'
+
+    @property
+    def part2(self):
+        return self.oxygen_generator_rating * self.c02_scrubber_rating
 
 
 def main():
     diag = SubDiagnostics()
     for line in yield_file('input'):
         diag.consume_line(line)
-    print(diag.diagnostics)
-    print(diag.result)
+    print('------')
+    print(diag.part1_diagnostics)
+    print(diag.part1)
+    print('------')
+    print(diag.part2_diagnostics)
+    print(diag.part2)
 
 
 if __name__ == '__main__':
