da3a759cc842bd72988e18fe63cb52a3cc12ceb3
Ben Felder <172955+kajk@users.noreply.github.com>
2021-12-07 07:31:45 +0100

    07: Fix part1

--- 07/crab_alignment.py
+++ 07/crab_alignment.py
@@ -9,19 +9,17 @@ def calculate_fuel_requirement(crab_positions, target) -> int:
 
 
 def brute_force_result(crab_positions) -> int:
-    most_efficient_target = None
     most_efficient_fuel_usage = None
     for target in range(min(crab_positions), max(crab_positions) + 1):
         fuel = calculate_fuel_requirement(crab_positions, target)
-        if most_efficient_target is None or fuel < most_efficient_fuel_usage:
-            most_efficient_target = target
+        if most_efficient_fuel_usage is None or fuel < most_efficient_fuel_usage:
             most_efficient_fuel_usage = fuel
-        print(f'{target} -> Fuel: {fuel}  (Efficient={most_efficient_target:})')
-    return most_efficient_target
+        print(f'{target} -> Fuel: {fuel}  (Efficient={most_efficient_fuel_usage})')
+    return most_efficient_fuel_usage
 
 
 def main():
-    with open('test.input') as fp:
+    with open('input') as fp:
         crab_positions = [int(elm.strip()) for elm in fp.readline().split(',')]
         result = brute_force_result(crab_positions)
         print(f'cheapest possible outcome: {result}')
