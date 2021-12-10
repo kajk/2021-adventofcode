6f3ee0b1a131b0a7636f6b769d70f93dcd6ab571
Ben Felder <172955+kajk@users.noreply.github.com>
2021-12-10 09:17:01 +0100

    10: part2

--- 10/parser.py
+++ 10/parser.py
@@ -34,14 +34,45 @@ def color(value: str):
     return Fore.CYAN + value + Style.RESET_ALL
 
 
-SCORE_MAP = {
+def main():
+    SCORE_MAP = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+    scores = []
+    for line in yield_file('input'):
+        chunks = []
+        # print(line)
+        for elm in line:
+            if elm in ['(', '[', '{', '<']:
+                chunks.append(Chunk(elm))
+            else:
+                if chunks[-1].is_valid_closing(elm):
+                    del chunks[-1]
+                else:
+                    # print(f'{color(line)} - Expected {color(chunks[-1].expected_closing)}, but found {color(elm)} instead.')
+                    chunks = []
+                    break
+        if len(chunks) > 0:
+            completion = ''.join(c.expected_closing for c in reversed(chunks))
+            score = 0
+            for e in completion:
+                score = (score * 5) + SCORE_MAP[e]
+            print(f'{color(line)}  - Complete by adding {color(completion)}. {score=}')
+            scores.append(score)
+    result = sorted(scores)[int(len(scores) / 2)]
+    print(f'{result=}')
+
+
+def main_part1():
+    SCORE_MAP = {
         ')': 3,
         ']': 57,
         '}': 1197,
         '>': 25137
-}
-
-def main():
+    }
     result = 0
     for line in yield_file('input'):
         chunks = []
